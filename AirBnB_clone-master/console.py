459cc4b01d1bbe9f628f6fbb11042e38ed3ef56a
NiteshHalai <nitesh.halai@gmail.com>
2023-02-10 00:58:40 +0300

    Update console.py

--- AirBnB_clone-master/console.py
+++ AirBnB_clone-master/console.py
@@ -4,6 +4,7 @@ HBNBCommand contains the entry point of the command interpreter
 
 import cmd
 import sys
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
 
@@ -31,6 +32,28 @@ class HBNBCommand(cmd.Cmd):
     def help_EOF(self):
         print ("syntax: EOF")
         print ("-- terminates the application")
+
+    def do_create(self, args):
+        
+        if len(args) == 0:
+            print("** class name missing **")
+        else:
+            args_to_list = args.split()
+            if args[0] == 'BaseModel':
+                my_model = BaseModel()
+                print (my_model.id())
+
+    def do_show(self, args):
+        if len(args) == 0:
+            print("** class name missing **")
+
+    def do_destroy(self, args):
+        if len(args) == 0:
+            print("** class name missing **")
+
+    def do_all(self, args):
+        if len(args) == 0:
+            print("** class name missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
